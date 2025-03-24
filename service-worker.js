d1d0cec1f0b956cb65140cdabd9d26a08b20ee4c
abomkdad <abomkdad2@gmail.com>
2025-03-24 19:50:06 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,37 @@
-// service-worker.js
-const CACHE_NAME = 'order-app-cache-v1';
+
+const CACHE_NAME = 'order-system-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
-                    .map((cacheName) => caches.delete(cacheName))
+                cacheNames.filter(cacheName => {
+                    return cacheName !== CACHE_NAME;
+                }).map(cacheName => caches.delete(cacheName))
             );
         })
     );
